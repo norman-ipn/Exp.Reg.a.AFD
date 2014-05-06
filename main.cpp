9402afa85a4af0ad5653bc50d4a38138d087530b
Norman <norman_escom_ipn@yahoo.com.mx>
2014-05-06 13:10:40 -0500

    Se creo el archivo fuente main.cpp para tener una base del código fuente.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * nombre del archivo: main.cpp
+ *
+ * creador: Rafael Norman Saucedo Delgado
+ *
+ * Objetivo: 
+ *   es un archivo de muestra base para la práctica. 
+ * 
+ * */
+
+int main(void)
+{
+   return 0;
+}
+
